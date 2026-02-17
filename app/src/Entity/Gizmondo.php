1ba8c78e7ea76eac3b6c280627df7e544fdfc50e
Caroline BIEHLER <70326049+Caroline1609@users.noreply.github.com>
2026-02-17 11:16:56 +0100

    upload

---
+++ app/src/Entity/Gizmondo.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\GizmondoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GizmondoRepository::class)]
+#[ApiResource]
+class Gizmondo
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $game = null;
+
+    #[ORM\Column(length: 4)]
+    private ?string $year = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $dev = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getGame(): ?string
+    {
+        return $this->game;
+    }
+
+    public function setGame(string $game): static
+    {
+        $this->game = $game;
+
+        return $this;
+    }
+
+    public function getYear(): ?string
+    {
+        return $this->year;
+    }
+
+    public function setYear(string $year): static
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getDev(): ?string
+    {
+        return $this->dev;
+    }
+
+    public function setDev(string $dev): static
+    {
+        $this->dev = $dev;
+
+        return $this;
+    }
+}
